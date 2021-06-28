0da1a6895c7018fc95e09b4731e34dcfcdb6ca21
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-28 11:28:22 +0200

    interaction updated

--- js/script.js
+++ js/script.js
@@ -4,27 +4,27 @@ const welcome = () => {
 {               
         const newBackgroundChange = () => {
                 const body = document.querySelector(".body");
-                body.classList.toggle(".newBackgroundChange");
+                body.classList.toggle("newBackgroundChange");
             };
         
-        // const onHideBackgroundClick = () => {
-        //         const header = document.querySelector(".js-header");
-        //         const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
+        const hideHeader = () => {
+                const header = document.querySelector(".js-header");
+                const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
         
-        //         header.classList.toggle();
-        //         if (header.classList.contains("hideHeaderButton")) {
-        //                 hideHeaderButton.innerText = "Pokaż nagłówek";
+                header.classList.toggle(hideHeader);
+                if (header.classList.contains(hideHeader)) {
+                        hideHeaderButton.innerText = "Ukryj nagłówek";
         
-        //         } else { hideHeaderButton.innerText = "Ukryj nagłówek;"}
-        // };
+                } else { hideHeaderButton.innerText = "Pokaż nagłówek"; }
+        };
             
             
            const init = () => {
                    const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-                //    const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
+                   const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
         
-                js-changeBackgroundButton.addEventListener("click", newBackgroundChange);
-                //    hideHeaderButton.addEventListener("click", hideHeaderButton);
+                changeBackgroundButton.addEventListener("click", newBackgroundChange);
+                hideHeaderButton.addEventListener("click", hideHeaderButton);
            };
  
 init();
