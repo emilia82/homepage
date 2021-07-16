0621fae395fdabbdd53740565186555b37c68347
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-16 15:47:54 +0200

    interaction in script added

--- js/script.js
+++ js/script.js
@@ -1,33 +1,47 @@
 const welcome = () => {
         console.log("Cześć! Witaj na mojej stronie! Poznaj moją twórczość!");
 }
-{               
+{
         const newBackgroundChange = () => {
                 const body = document.querySelector(".body");
                 body.classList.toggle("newBackgroundChange");
-            };
-        
+        };
+
         const hideHeader = () => {
                 const header = document.querySelector(".js-header");
                 const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
-        
+
                 header.classList.toggle("hidenHeader");
                 if (header.classList.contains("hidenHeader")) {
                         hideHeaderButton.innerText = "Pokaż nagłówek";
-        
+
                 } else { hideHeaderButton.innerText = "Ukryj nagłówek"; }
         };
-            
-            
-           const init = () => {
-                   const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-                   const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
-        
+
+
+        const init = () => {
+                const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+                const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
+
                 changeBackgroundButton.addEventListener("click", newBackgroundChange);
                 hideHeaderButton.addEventListener("click", hideHeader);
-           };
- 
-init();
-(welcome);
+        };
+
+        let myImage = document.querySelector("img");
+        myImage.onclick = function () {
+                let mySrc = myImage.getAttribute("src");
+                if (mySrc === "image/EmiliaWarakomska.jpg") {
+                        myImage.setAttribute("src", "image/Emilia_Warakomska.jpg");
+                } else {
+                        myImage.setAttribute("src", "image/Emilia_Warakomska.jpg");
+                }
+        }
+
+        init();
+        (welcome);
+
+
+
+
 }
 
