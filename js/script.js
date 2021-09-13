1ddb446ca7f055a1315ef577fa304604994a0eaf
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-13 14:41:54 +0200

    changed name of Background interaction

--- js/script.js
+++ js/script.js
@@ -2,32 +2,35 @@ const welcome = () => {
         console.log("Cześć! Witaj na mojej stronie! Poznaj moją twórczość!");
 }
 {
-        const newBackgroundChange = () => {
+        
+        
+        const toggleBackground = () => {
                 const body = document.querySelector(".body");
-                body.classList.toggle("newBackgroundChange");
+                body.classList.toggle("toggleBackground");
         };
 
         const hideHeader = () => {
                 const header = document.querySelector(".js-header");
                 const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
 
-                header.classList.toggle("hidenHeader");
-                if (header.classList.contains("hidenHeader")) {
+                header.classList.toggle("hiddenHeader");
+                if (header.classList.contains("hiddenHeader")) {
                         hideHeaderButton.innerText = "Pokaż nagłówek";
 
                 } else { hideHeaderButton.innerText = "Ukryj nagłówek"; }
         };
 
-
         const init = () => {
                 const changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
                 const hideHeaderButton = document.querySelector(".js-hideHeaderButton");
 
-                changeBackgroundButton.addEventListener("click", newBackgroundChange);
+                changeBackgroundButton.addEventListener("click", toggleBackground);
                 hideHeaderButton.addEventListener("click", hideHeader);
         };
 
-        let myImage = document.querySelector("img");
+       
+        const myImage = document.querySelector("img");
+        
         myImage.onclick = function () {
                 let mySrc = myImage.getAttribute("src");
                 if (mySrc === "image/EmiliaWarakomska.jpg") {
@@ -40,8 +43,6 @@ const welcome = () => {
         init();
         (welcome);
 
+};
 
-
-
-}
 
